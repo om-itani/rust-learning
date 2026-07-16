1de12b84e165f81acfad6341f76d86f6b2a986c7
Omar Itani <omar.itaniau@gmail.com>
2026-07-16 21:23:35 +1000

    Day 2: Celsius to Fahrenheit program

---
+++ Day 2/CelsiusToFahrenheit.rs
@@ -0,0 +1,6 @@
+fn main() {
+    let celsius: f64 = 25.0; // Declares a read-only 8-byte variable
+    let fahrenheit: f64 = (celsius * 9.0 / 5.0) + 32.0;
+
+    println!("Fahrenheit: {}", fahrenheit);
+}
